5b4caa0e6c58ca86d73802a9d263defec4a9273b
FrogIT <kimhg0149@gmail.com>
2023-07-30 02:23:24 +0000

    personality 추가

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const btop = $('#project').offset().top
 
 $(window).ready(function() {
     const date = new Date().toISOString().split('T')[0].split('-')
-    $('#today').html(`${date[0]}.<span class="dot7">${date[1]}.${date[2]}.</span>`)
-
+    $('#today').html(`${date[0]}.<span class="dot7">${date[1]}.${date[2]}. 현재</span>`)
+    const traces = [1,2,3,4,5]
+    traces.forEach((v) => {
+        $(`#get-time-since${v}`).html(getTimeSince($(`#get-time-since${v}`).text()))
+    })
+    
     /*jQuery(function($) {
         $('#rss-feeds').rss('https://rss.blog.naver.com/kim04099',
         {
@@ -49,15 +53,15 @@ var a = 0
 
 function getTimeSince(date) {
     var sec = Math.floor((new Date() - new Date(date)) / 1000)
-    var int = Math.floor(sec / 604800)
+    var int = Math.floor(sec / 31536000)
     if (int >= 1) {
-        return int + '주 전'
+        return int + '년 전'
     }
-    int = Math.floor(sec / 86400);
+    int = Math.floor(sec / 2628000);
     if (int >= 1) {
-        return int + '일 전'
+        return int + '개월 전'
     }
-    return '<span style="color:red">NEW</span>'
+    return 'NEW'
 }
 
 function scrollAnimation() {
